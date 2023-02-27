6bcf9d01e802245021ab94853d80ab58f6778565
Vidisha Vijay Sawant <vidishasawantv@gmail.com>
2023-02-27 13:53:54 -0500

    update

--- Typescriptdemo/datatypes.ts
+++ Typescriptdemo/datatypes.ts
@@ -3,3 +3,11 @@ Lname = 'Vidisha';
 let newname = Lname.toLowerCase();
 console.log(newname);
 //Lname = 10;
+let age : number;
+age = 20;
+
+let dob = "25";
+let result = parseInt(dob);
+
+let isValid : boolean = false;
+console.log(isValid);
