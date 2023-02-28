0d8e348717e8be31a19008c4cf2762bffc369690
Vidisha Vijay Sawant <vidishasawantv@gmail.com>
2023-02-28 12:46:48 -0500

    Update datatypes.ts

--- Typescriptdemo/datatypes.ts
+++ Typescriptdemo/datatypes.ts
@@ -11,3 +11,15 @@ let result = parseInt(dob);
 
 let isValid : boolean = false;
 console.log(isValid);
+
+let emplist : string[];
+emplist = ["Vidisha", "Varsha", "Rajeev"];
+
+let numlist : Array<number>;
+numlist =[1,2,3,4,56,6];
+
+let results = numlist.filter((num) => num >2);
+let result1 = numlist.find((num) => num == 2);
+console.log(results);
+
+
